48ad791e5f0d71c50eca0cea2146003746699d28
Florian Papsdorf <florian.papsdorf@sap.com>
2021-02-24 12:14:13 +0100

    Remove obsolete toast configuration type

--- app/src/types/ToastConfiguration.ts
+++
@@ -1,8 +0,0 @@
-import { ToastType } from 'types/ToastTypes';
-
-export interface ToastConfiguration {
-  // if id == -1, we assume that it is only a prototype of a toast
-  id: number;
-  message: string;
-  type: ToastType;
-}
