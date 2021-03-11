c144125acf6474386b8e49ee439a27838f648e1f
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-03-11 18:31:02 +0100

    Improve naming

--- app/src/store/reducers/SelectableInputReducer.ts
+++ app/src/store/reducers/SelectableInputReducer.ts
@@ -2,19 +2,19 @@ import { SelectableInputStoreActionTypes } from 'store/actions/actionTypes';
 import { SnowmanAction } from 'store/messages';
 import { SelectableInputStore } from 'store/models';
 
-const initialValues: SelectableInputStore = {
+const initialState: SelectableInputStore = {
   searchString: '',
   eventPopover: undefined,
   shouldShowPopover: false,
 };
 
 export const SelectableInputReducer = (
-  state: SelectableInputStore = initialValues,
+  state: SelectableInputStore = initialState,
   action: SnowmanAction
 ): SelectableInputStore => {
   switch (action.type) {
     case SelectableInputStoreActionTypes.RESET_COMPONENT:
-      return initialValues;
+      return initialState;
     case SelectableInputStoreActionTypes.CHANGE_SEARCH_STRING:
       return { ...state, searchString: action.payload as string };
     case SelectableInputStoreActionTypes.SHOW_POPOVER:
