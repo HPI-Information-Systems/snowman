7f3a543f08528aab6ad5abfcf131ce934bccebd0
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-03-16 17:01:22 +0100

    Handle primitive drag n drop

--- app/src/store/reducers/ExperimentsReducer.ts
+++ app/src/store/reducers/ExperimentsReducer.ts
@@ -1,5 +1,5 @@
 import { Experiment } from 'api';
-import { difference } from 'lodash';
+import { difference, nth } from 'lodash';
 import { ExperimentsStoreActionTypes as actionTypes } from 'store/actions/actionTypes';
 import { SnowmanAction } from 'store/messages';
 import { ExperimentsStore } from 'store/models';
@@ -27,6 +27,19 @@ const getBucketFromId = (
       return state.chosenGoldStandards;
   }
 };
+
+const insertExperimentAt = (
+  sourceList: Experiment[],
+  anExperiment: Experiment,
+  targetIndex: number
+): Experiment[] =>
+  sourceList.length === 0
+    ? [anExperiment]
+    : [
+        ...sourceList.splice(0, targetIndex),
+        anExperiment,
+        ...sourceList.slice(targetIndex),
+      ];
 
 export const ExperimentsReducer = (
   state: ExperimentsStore = initialState,
@@ -66,12 +79,56 @@ export const ExperimentsReducer = (
         ),
       };
     case actionTypes.DRAG_N_DROP_EXPERIMENT: {
+      let chosenExperiments,
+        chosenGoldstandards,
+        availableExperiments: Experiment[];
       const eventDescriptor: DragNDropDescriptor = action.payload as DragNDropDescriptor;
-      const [draggedExperiment]: Experiment[] = getBucketFromId(
-        state,
-        eventDescriptor.sourceBucket
-      ).splice(eventDescriptor.sourceIndex, 1);
-      return state;
+      const draggedExperiment: Experiment | undefined = nth(
+        getBucketFromId(state, eventDescriptor.sourceBucket),
+        eventDescriptor.sourceIndex
+      );
+      if (draggedExperiment === undefined) return state;
+      chosenGoldstandards = state.chosenGoldStandards.filter(
+        (anExperiment: Experiment): boolean =>
+          anExperiment.id !== draggedExperiment.id
+      );
+      chosenExperiments = state.chosenExperiments.filter(
+        (anExperiment: Experiment): boolean =>
+          anExperiment.id !== draggedExperiment.id
+      );
+      availableExperiments = state.availableExperiments.filter(
+        (anExperiment: Experiment): boolean =>
+          anExperiment.id !== draggedExperiment.id
+      );
+      switch (eventDescriptor.targetBucket) {
+        case ExperimentBuckets.CHOSEN_GOLDSTANDARDS:
+          chosenGoldstandards = insertExperimentAt(
+            chosenGoldstandards,
+            draggedExperiment,
+            eventDescriptor.targetIndex
+          );
+          break;
+        case ExperimentBuckets.CHOSEN_EXPERIMENTS:
+          chosenExperiments = insertExperimentAt(
+            chosenExperiments,
+            draggedExperiment,
+            eventDescriptor.targetIndex
+          );
+          break;
+        case ExperimentBuckets.AVAILABLE_EXPERIMENTS:
+          availableExperiments = insertExperimentAt(
+            availableExperiments,
+            draggedExperiment,
+            eventDescriptor.targetIndex
+          );
+          break;
+      }
+      return {
+        ...state,
+        chosenExperiments: chosenExperiments,
+        chosenGoldStandards: chosenGoldstandards,
+        availableExperiments: availableExperiments,
+      };
     }
     case actionTypes.RESET_SELECTED_EXPERIMENTS:
       return {
