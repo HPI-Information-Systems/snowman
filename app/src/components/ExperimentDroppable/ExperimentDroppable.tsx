662dffd5ee1afa959977797f057d5a1bb3e75567
phpfs <phpfs@users.noreply.github.com>
2021-03-17 16:47:40 +0100

    Use clones to render draggable objects

--- app/src/components/ExperimentDroppable/ExperimentDroppable.tsx
+++ app/src/components/ExperimentDroppable/ExperimentDroppable.tsx
@@ -6,49 +6,67 @@ import React from 'react';
 import {
   Draggable,
   DraggableProvided,
+  DraggableRubric,
+  DraggableStateSnapshot,
   Droppable,
   DroppableProvided,
 } from 'react-beautiful-dnd';
 
+const getRenderItem = (allExperiments: Experiment[]) =>
+  function renderedItem(
+    provided: DraggableProvided,
+    snapshot: DraggableStateSnapshot,
+    rubric: DraggableRubric
+  ): JSX.Element {
+    const anExperiment = allExperiments[rubric.source.index];
+    if (anExperiment === undefined)
+      throw Error('referenced non-existent experiment');
+
+    return (
+      <IonRow
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        {...provided.draggableProps}
+      >
+        <IonCol>
+          <OptionCard
+            key={'card' + anExperiment.id}
+            title={anExperiment.name}
+            subtitle={'anOption.algorithmId'}
+            description={anExperiment.description}
+            tags={[]}
+            isSelected={true}
+          />
+        </IonCol>
+      </IonRow>
+    );
+  };
+
 const ExperimentDroppable = ({
   bucketId,
   bucketContent,
-}: ExperimentDroppableProps): JSX.Element => (
-  <Droppable droppableId={bucketId}>
-    {(provided: DroppableProvided): JSX.Element => (
-      <IonGrid {...provided.droppableProps} ref={provided.innerRef}>
-        {bucketContent.map(
-          (anExperiment: Experiment, index: number): JSX.Element => (
-            <Draggable
-              key={anExperiment.id}
-              draggableId={anExperiment.id.toString()}
-              index={index}
-            >
-              {(provided: DraggableProvided): JSX.Element => (
-                <IonRow
-                  ref={provided.innerRef}
-                  {...provided.dragHandleProps}
-                  {...provided.draggableProps}
-                >
-                  <IonCol>
-                    <OptionCard
-                      key={'card' + anExperiment.id}
-                      title={anExperiment.name}
-                      subtitle={'anOption.algorithmId'}
-                      description={anExperiment.description}
-                      tags={[]}
-                      isSelected={true}
-                    />
-                  </IonCol>
-                </IonRow>
-              )}
-            </Draggable>
-          )
-        )}
-        {provided.placeholder}
-      </IonGrid>
-    )}
-  </Droppable>
-);
+}: ExperimentDroppableProps): JSX.Element => {
+  const renderBucket = getRenderItem(bucketContent);
+  return (
+    <Droppable droppableId={bucketId} renderClone={renderBucket}>
+      {(provided: DroppableProvided): JSX.Element => (
+        <IonGrid {...provided.droppableProps} ref={provided.innerRef}>
+          {bucketContent.map(
+            (anExperiment: Experiment, index: number): JSX.Element => (
+              <Draggable
+                key={anExperiment.id}
+                draggableId={anExperiment.id.toString()}
+                index={index}
+              >
+                {renderBucket}
+              </Draggable>
+            )
+          )}
+          {provided.placeholder}
+        </IonGrid>
+      )}
+    </Droppable>
+  );
+};
 
 export default ExperimentDroppable;
