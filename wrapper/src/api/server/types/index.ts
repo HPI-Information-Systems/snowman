973519f606ffe82f9fa384497cf3e7da81e2bf9f
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-18 22:26:12 +0100

    adding autogenerated algorithmtypes

--- wrapper/src/api/server/types/index.ts
+++ wrapper/src/api/server/types/index.ts
@@ -2,6 +2,7 @@ export * from './Algorithm';
 export * from './AlgorithmAllOf';
 export * from './AlgorithmId';
 export * from './AlgorithmValues';
+export * from './AlgorithmValuesSoftKPIs';
 export * from './Dataset';
 export * from './DatasetAllOf';
 export * from './DatasetId';
