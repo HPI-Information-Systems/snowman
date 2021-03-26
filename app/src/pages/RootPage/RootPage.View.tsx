dd45e8e953cd3f1f5bdb09ce6a99374488739d82
phpfs <phpfs@users.noreply.github.com>
2021-03-26 11:50:33 +0100

    Revert to old root page

--- app/src/pages/RootPage/RootPage.View.tsx
+++ app/src/pages/RootPage/RootPage.View.tsx
@@ -1,43 +1,27 @@
 import 'pages/RootPage/RootPageStyle.css';
 
-import { IonCard, IonCardContent } from '@ionic/react';
 import PageStruct from 'components/PageStruct/PageStruct';
-import { VennFourSetsExample } from 'components/VennDiagram/venn/types/fourSetsTypes';
-import { VennThreeSetsExample } from 'components/VennDiagram/venn/types/threeSetsTypes';
-import { VennTwoSetsExample } from 'components/VennDiagram/venn/types/twoSetsTypes';
-import { VennDiagram } from 'components/VennDiagram/VennDiagram';
-import { VennDiagramFlavors } from 'components/VennDiagram/VennDiagramProps';
 import React from 'react';
 
-const RootPageView = (): JSX.Element => {
-  return (
-    <PageStruct title="Home Page" showNextFab={true}>
-      <IonCard>
-        <IonCardContent>
-          <VennDiagram
-            flavor={VennDiagramFlavors.FourSets}
-            sets={VennFourSetsExample}
-          />
-        </IonCardContent>
-      </IonCard>
-      <IonCard>
-        <IonCardContent>
-          <VennDiagram
-            flavor={VennDiagramFlavors.ThreeSets}
-            sets={VennThreeSetsExample}
-          />
-        </IonCardContent>
-      </IonCard>
-      <IonCard>
-        <IonCardContent>
-          <VennDiagram
-            flavor={VennDiagramFlavors.TwoSets}
-            sets={VennTwoSetsExample}
-          />
-        </IonCardContent>
-      </IonCard>
-    </PageStruct>
-  );
-};
+const RootPageView = (): JSX.Element => (
+  <PageStruct title="Home Page" showNextFab={true}>
+    <div className="ion-text-center introduction">
+      <img src="./logo192.png" alt="logo" />
+      <h1>Welcome to Snowman Benchmark!</h1>
+      <p>
+        Start by selecting a dataset to work with on the left of this page.
+        <br />
+        You will be guided through the process necessary to create your first
+        analysis!
+      </p>
+      <p>
+        This app is open-source and community-driven.
+        <br />
+        Contributions of all kinds are welcome - feel free to fork us on Github!
+        :)
+      </p>
+    </div>
+  </PageStruct>
+);
 
 export default RootPageView;
