338df57055d75859bef692364632de0e8069732d
phpfs <phpfs@users.noreply.github.com>
2021-03-26 12:35:15 +0100

    Revert: Disable getAlgorithms for initial state

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,7 +1,7 @@
 import AppView from 'app/App.View';
 import { AppDispatchProps, AppStateProps } from 'app/AppProps';
 import { connect } from 'react-redux';
-//import { getAlgorithms } from 'store/actions/AlgorithmsPageActions';
+import { getAlgorithms } from 'store/actions/AlgorithmsPageActions';
 import { SnowmanDispatch } from 'store/messages';
 import { Store } from 'store/models';
 
@@ -11,8 +11,7 @@ const mapStateToProps = (state: Store): AppStateProps => ({
 
 const mapDispatchToProps = (dispatch: SnowmanDispatch): AppDispatchProps => ({
   loadInitialState() {
-    // Todo: Enable this again
-    //dispatch(getAlgorithms()).then();
+    dispatch(getAlgorithms()).then();
   },
 });
 
