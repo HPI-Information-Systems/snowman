2bdadaf405be0d3fa949cd6be3d8cbf7231630ea
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-03-30 21:39:09 +0200

    Implement generic helpers

--- app/src/utils/dragNDropHelpers.ts
+++ app/src/utils/dragNDropHelpers.ts
@@ -1,5 +1,6 @@
 import { DropResult } from 'react-beautiful-dnd';
 import { DragNDropDescriptor } from 'types/DragNDropDescriptor';
+import { ObjectWithId } from 'types/ObjectWithId';
 
 export const getDndDescriptorFromDropResult = <T extends string>(
   aDropResult: DropResult,
@@ -10,3 +11,24 @@ export const getDndDescriptorFromDropResult = <T extends string>(
   targetBucket: (aDropResult.destination?.droppableId ?? fallbackBucket) as T,
   targetIndex: aDropResult.destination?.index ?? 0,
 });
+
+export const insertElementAt = <T>(
+  sourceList: T[],
+  anElement: T,
+  targetIndex: number
+): T[] =>
+  sourceList.length === 0
+    ? [anElement]
+    : [
+        ...sourceList.slice(0, targetIndex),
+        anElement,
+        ...sourceList.slice(targetIndex),
+      ];
+
+export const filterOutAnElement = <T extends ObjectWithId>(
+  aBucket: T[],
+  anElement: T
+): T[] =>
+  aBucket.filter(
+    (currentElement: T): boolean => currentElement.id !== anElement.id
+  );
