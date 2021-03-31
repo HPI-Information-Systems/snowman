2d61b05d631a7682514ee0cb4cdb310d9326673f
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-31 18:51:47 +0200

    migration script for algorithm

---
+++ wrapper/src/api/database/schemas/migrations/v4.ts
@@ -0,0 +1,47 @@
+import { tables } from '../../';
+import { SetupOptions } from '../../setup';
+import { databaseBackend } from '../../setup/backend';
+import { SchemaVersion } from './schemaVersion';
+import { SchemaV2 } from './v2';
+
+export class SchemaV3 extends SchemaVersion {
+  readonly predecessor = new SchemaV2();
+
+  protected async migrateFromLastVersion(options: SetupOptions): Promise<void> {
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+         DROP COLUMN
+            timeToConfigure`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+         ADD COLUMN
+            useCase TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+         ADD COLUMN
+            inputFormat TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+         ADD COLUMN
+            interface TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+            costs INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+            os TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+         ADD COLUMN
+             timeToConfigure INTEGER`
+    );
+  }
+}
