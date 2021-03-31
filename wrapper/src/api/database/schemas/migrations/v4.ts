9c09055c0343b5aaaf6ef1f38b27d9960f998c44
lukas.laskowski <lukas.laskowski@siemens.com>
2021-03-31 19:20:25 +0200

    updating to migration v4

--- wrapper/src/api/database/schemas/migrations/v4.ts
+++ wrapper/src/api/database/schemas/migrations/v4.ts
@@ -2,10 +2,10 @@ import { tables } from '../../';
 import { SetupOptions } from '../../setup';
 import { databaseBackend } from '../../setup/backend';
 import { SchemaVersion } from './schemaVersion';
-import { SchemaV2 } from './v2';
+import { SchemaV3 } from './v3';
 
-export class SchemaV3 extends SchemaVersion {
-  readonly predecessor = new SchemaV2();
+export class SchemaV4 extends SchemaVersion {
+  readonly predecessor = new SchemaV3();
 
   protected async migrateFromLastVersion(options: SetupOptions): Promise<void> {
     databaseBackend().exec(
