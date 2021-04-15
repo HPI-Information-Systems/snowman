3acc869a69c7fa1bf04fc28d84637fdb848b8d9f
lukas.laskowski <lukas.laskowski@siemens.com>
2021-04-15 19:35:50 +0200

    changing migration to new softKPIs

--- wrapper/src/api/database/schemas/migrations/v5.ts
+++ wrapper/src/api/database/schemas/migrations/v5.ts
@@ -13,5 +13,65 @@ export class SchemaV5 extends SchemaVersion {
            ADD COLUMN
                timeToConfigure INTEGER`
     );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.experiment.schema.name}
+           ADD COLUMN
+               expertiseLevel INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+               integrationTime INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              generalCosts INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              matchingSolutionExpertise INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              matchingSolutionHrAmount INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              domainExpertise INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              domainHrAmount INTEGER`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              interfaces TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              supportedOS TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              deploymentType TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              solutionType TEXT`
+    );
+    databaseBackend().exec(
+      `ALTER TABLE ${tables.meta.algorithm.schema.name}
+           ADD COLUMN
+              useCase TEXT`
+    );
   }
 }
