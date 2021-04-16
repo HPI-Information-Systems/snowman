3f8096e1da8058113b179fcc052da760336f72e8
lukas.laskowski <lukas.laskowski@siemens.com>
2021-04-16 09:37:37 +0200

    adding further efforts

---
+++ wrapper/src/api/providers/algorithm/util/effortPoints/logarthimicEffort.ts
@@ -0,0 +1,10 @@
+import { BaseEffort } from './baseEffort';
+
+export class LogarithmicEffort extends BaseEffort {
+  name = 'logarithmic expertise level';
+  range: [number, number] = [0, this.expertise + this.hrAmount];
+  formula = '$$log(expertise level) * HR-Amount$$';
+  get value(): number {
+    return Math.log(this.expertise) * this.hrAmount;
+  }
+}
