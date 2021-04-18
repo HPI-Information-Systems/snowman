89a7a23dba2a11cfa5fb0aa0617f65daf0a57714
phpfs <phpfs@users.noreply.github.com>
2021-04-18 20:43:21 +0200

    Refactor to remove code duplication

--- app/src/components/TabBar/TabBar.View.tsx
+++ app/src/components/TabBar/TabBar.View.tsx
@@ -9,49 +9,35 @@ import styles from 'components/TabBar/TabBarStyles.module.css';
 import React from 'react';
 import style from 'theme/style';
 
-const TabBarView = ({ openSubApp, activeSubApp }: TabBarProps): JSX.Element => (
-  <div className={styles.toolbar}>
-    <div className={styles.toolbarContainer}>
-      <IonButtons slot="start" class={styles.buttonContainer}>
-        {TabBarConfigLeft.map(
-          (aTabConfigItem: TabBarConfigItem): JSX.Element => (
-            <IonButton
-              key={aTabConfigItem.viewID}
-              fill="clear"
-              size="large"
-              className={
-                activeSubApp === aTabConfigItem.viewID
-                  ? style(styles.buttonElement, styles.active)
-                  : style(styles.buttonElement)
-              }
-              onClick={(): void => openSubApp(aTabConfigItem.viewID)}
-            >
-              {aTabConfigItem.title}
-            </IonButton>
-          )
-        )}
-      </IonButtons>
-      <IonButtons slot="end" class={styles.buttonContainer}>
-        {TabBarConfigRight.map(
-          (aTabConfigItem: TabBarConfigItem): JSX.Element => (
-            <IonButton
-              key={aTabConfigItem.viewID}
-              fill="clear"
-              size="large"
-              className={
-                activeSubApp === aTabConfigItem.viewID
-                  ? style(styles.buttonElement, styles.active)
-                  : style(styles.buttonElement)
-              }
-              onClick={(): void => openSubApp(aTabConfigItem.viewID)}
-            >
-              {aTabConfigItem.title}
-            </IonButton>
-          )
-        )}
-      </IonButtons>
+const TabBarView = ({ openSubApp, activeSubApp }: TabBarProps): JSX.Element => {
+  const renderButtonFor = (aTabConfigItem: TabBarConfigItem): JSX.Element => (
+    <IonButton
+      key={aTabConfigItem.viewID}
+      fill="clear"
+      size="large"
+      className={
+        activeSubApp === aTabConfigItem.viewID
+          ? style(styles.buttonElement, styles.active)
+          : style(styles.buttonElement)
+      }
+      onClick={(): void => openSubApp(aTabConfigItem.viewID)}
+    >
+      {aTabConfigItem.title}
+    </IonButton>
+  );
+
+  return (
+    <div className={styles.toolbar}>
+      <div className={styles.toolbarContainer}>
+        <IonButtons slot="start" class={styles.buttonContainer}>
+          {TabBarConfigLeft.map(renderButtonFor)}
+        </IonButtons>
+        <IonButtons slot="end" class={styles.buttonContainer}>
+          {TabBarConfigRight.map(renderButtonFor)}
+        </IonButtons>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TabBarView;
