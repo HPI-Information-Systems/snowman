e0fd268eb15d4d7e27691f67c841f9ba88380383
phpfs <phpfs@users.noreply.github.com>
2021-04-18 13:43:47 +0200

    Allow unknown as ActionPayload

--- app/src/store/messages.ts
+++ app/src/store/messages.ts
@@ -14,6 +14,7 @@ import { DragNDropDescriptor } from 'types/DragNDropDescriptor';
 import experimentFileFormatEnum from 'types/ExperimentFileFormats';
 
 type ActionPayload =
+  | unknown
   | Metric[]
   | Metric[][]
   | FileResponse
