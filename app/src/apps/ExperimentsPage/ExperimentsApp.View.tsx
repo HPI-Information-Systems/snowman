3a5eb2f9645cda8001c197f90b261961b0f12cb7
phpfs <phpfs@users.noreply.github.com>
2021-04-19 16:27:18 +0200

    Create filters for ExperimentsApp.View.tsx

--- app/src/apps/ExperimentsPage/ExperimentsApp.View.tsx
+++ app/src/apps/ExperimentsPage/ExperimentsApp.View.tsx
@@ -1,7 +1,16 @@
 import 'apps/ExperimentsPage/ExperimentsAppStyles.css';
 
-import { IonCol, IonGrid, IonRow } from '@ionic/react';
-import { Experiment } from 'api';
+import {
+  IonCol,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+} from '@ionic/react';
+import { Algorithm, Dataset, Experiment } from 'api';
 import { ExperimentsAppProps } from 'apps/ExperimentsPage/ExperimentsAppProps';
 import ExperimentCard from 'components/ExperimentCard/ExperimentCard';
 import React from 'react';
@@ -18,6 +27,55 @@ const ExperimentsAppView = ({
 }: ExperimentsAppProps): JSX.Element => {
   return (
     <>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel>Filter by datasets</IonLabel>
+              <IonSelect
+                value={selectedDatasets}
+                placeholder="Select multiple"
+                multiple={true}
+              >
+                {datasets.map(
+                  (aDataset: Dataset): JSX.Element => (
+                    <IonSelectOption
+                      key={`filter_datasets_${aDataset.id}`}
+                      value={aDataset.id}
+                    >
+                      {aDataset.name}
+                    </IonSelectOption>
+                  )
+                )}
+              </IonSelect>
+            </IonItem>
+          </IonCol>
+          <IonCol>
+            <IonItem>
+              <IonLabel>Filter by matching solutions</IonLabel>
+              <IonSelect
+                value={selectedAlgorithms}
+                placeholder="Select multiple"
+                multiple={true}
+              >
+                {algorithms.map(
+                  (anAlgorithm: Algorithm): JSX.Element => (
+                    <IonSelectOption
+                      key={`filter_algorithms_${anAlgorithm.id}`}
+                      value={anAlgorithm.id}
+                    >
+                      {anAlgorithm.name}
+                    </IonSelectOption>
+                  )
+                )}
+              </IonSelect>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonText color="primary">
+        <h3>Experiments</h3>
+      </IonText>
       <IonGrid>
         <IonRow>
           {experiments.map((anExperiment: Experiment) => (
