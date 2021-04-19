bfd591bc9d1ab89086d4c54c968e8b4c03e9436c
phpfs <phpfs@users.noreply.github.com>
2021-04-19 11:54:59 +0200

    Build back old ExperimentsPage

--- app/src/pages/ExperimentsPage/ExperimentsPage.View.tsx
+++ app/src/pages/ExperimentsPage/ExperimentsPage.View.tsx
@@ -1,117 +1,36 @@
 import 'pages/ExperimentsPage/ExperimentsPageStyles.css';
 
-import {
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonChip,
-  IonCol,
-  IonGrid,
-  IonIcon,
-  IonLabel,
-  IonRow,
-} from '@ionic/react';
-import { Algorithm } from 'api';
+import { IonCol, IonGrid, IonRow } from '@ionic/react';
+import { Experiment } from 'api';
 import AddExperimentFab from 'components/AddFab/AddExperimentFab';
+import ExperimentCard from 'components/ExperimentCard/ExperimentCard';
 import ExperimentDialog from 'components/ExperimentDialog/ExperimentDialog';
-import ExperimentDroppable from 'components/ExperimentDroppable/ExperimentDroppable';
 import ExperimentPreviewer from 'components/FilePreviewer/ExperimentPreviewer';
 import PageStruct from 'components/PageStructOLD/PageStruct';
-import { filterCircleOutline } from 'ionicons/icons';
 import { ExperimentsPageProps } from 'pages/ExperimentsPage/ExperimentsPageProps';
 import React, { useEffect } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { ExperimentBuckets } from 'types/ExperimentBuckets';
-import { isMatchingSolutionSelected } from 'utils/algorithmHelpers';
 
 const ExperimentsPageView = ({
   matchingSolutions,
   selectedMatchingSolutions,
-  clickOnMatchingSolution,
-  dragExperiment,
+  datasets,
+  selectedDatasets,
+  currentExperiments,
   loadExperiments,
-  showExperimentFilters,
-  clickOnExperimentFilterTool,
 }: ExperimentsPageProps): JSX.Element => {
   useEffect(loadExperiments, [loadExperiments]);
   return (
     <PageStruct title="Experiments Selector">
       <IonGrid>
         <IonRow>
-          <DragDropContext onDragEnd={dragExperiment}>
-            <IonCol size="6" sizeXl="4">
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardTitle className="dropzone-title">
-                    <span>Available Experiments</span>
-                    <IonIcon
-                      className="ion-icon"
-                      size="large"
-                      color={showExperimentFilters ? 'primary' : 'dark'}
-                      icon={filterCircleOutline}
-                      onClick={clickOnExperimentFilterTool}
-                    />
-                  </IonCardTitle>
-                </IonCardHeader>
-                {showExperimentFilters ? (
-                  <div className="ms-tags-container">
-                    {matchingSolutions.map(
-                      (aMatchingSolution: Algorithm): JSX.Element => (
-                        <IonChip
-                          color={
-                            isMatchingSolutionSelected(
-                              aMatchingSolution,
-                              selectedMatchingSolutions
-                            )
-                              ? 'primary'
-                              : 'dark'
-                          }
-                          outline={false}
-                          key={aMatchingSolution.id}
-                          onClick={(): void =>
-                            clickOnMatchingSolution(aMatchingSolution)
-                          }
-                        >
-                          <IonLabel>{aMatchingSolution.name}</IonLabel>
-                        </IonChip>
-                      )
-                    )}
-                  </div>
-                ) : null}
-                <ExperimentDroppable
-                  bucketId={ExperimentBuckets.AVAILABLE_EXPERIMENTS}
-                />
-              </IonCard>
+          {currentExperiments.map((anExperiment: Experiment) => (
+            <IonCol key={'col' + anExperiment.id} size="4" sizeXl="3">
+              <ExperimentCard
+                key={`experimentCard-${anExperiment.id}`}
+                experiment={anExperiment}
+              />
             </IonCol>
-            <IonCol size="6" sizeXl="8">
-              <IonRow>
-                <IonCol size="12" sizeXl="6" className="col-no-padding">
-                  <IonCard>
-                    <IonCardHeader>
-                      <IonCardTitle className="dropzone-title">
-                        <span>Selected Ground Truth</span>
-                      </IonCardTitle>
-                    </IonCardHeader>
-                    <ExperimentDroppable
-                      bucketId={ExperimentBuckets.CHOSEN_GOLDSTANDARDS}
-                    />
-                  </IonCard>
-                </IonCol>
-                <IonCol size="12" sizeXl="6" className="col-no-padding">
-                  <IonCard>
-                    <IonCardHeader>
-                      <IonCardTitle className="dropzone-title">
-                        <span>Selected Experiments</span>
-                      </IonCardTitle>
-                    </IonCardHeader>
-                    <ExperimentDroppable
-                      bucketId={ExperimentBuckets.CHOSEN_EXPERIMENTS}
-                    />
-                  </IonCard>
-                </IonCol>
-              </IonRow>
-            </IonCol>
-          </DragDropContext>
+          ))}
         </IonRow>
       </IonGrid>
       <AddExperimentFab />
--- app/src/pages/ExperimentsPage/ExperimentsPage.tsx
+++ app/src/pages/ExperimentsPage/ExperimentsPage.tsx
@@ -1,42 +1,26 @@
-import { Algorithm } from 'api';
 import ExperimentsPageView from 'pages/ExperimentsPage/ExperimentsPage.View';
 import {
   ExperimentsPageDispatchProps,
   ExperimentsPageStateProps,
 } from 'pages/ExperimentsPage/ExperimentsPageProps';
-import { DropResult } from 'react-beautiful-dnd';
 import { connect } from 'react-redux';
-import {
-  clickOnExperimentsFilterTool,
-  clickOnMatchingSolution,
-  dragNDropAnExperiment,
-  getExperiments,
-} from 'store/actions/ExperimentsPageActions';
+import { getExperiments } from 'store/actions/ExperimentsPageActions';
 import { SnowmanDispatch } from 'store/messages';
 import { Store } from 'store/models';
 
 const mapStateToProps = (state: Store): ExperimentsPageStateProps => ({
   matchingSolutions: state.CoreStore.algorithms,
-  selectedMatchingSolutions:
-    state.BenchmarkConfigurationStore.selectedMatchingSolutions,
-  showExperimentFilters:
-    state.BenchmarkConfigurationStore.showExperimentFilters,
+  selectedMatchingSolutions: [],
+  datasets: state.CoreStore.datasets,
+  selectedDatasets: [],
+  currentExperiments: state.CoreStore.experiments,
 });
 
 const mapDispatchToProps = (
   dispatch: SnowmanDispatch
 ): ExperimentsPageDispatchProps => ({
-  clickOnMatchingSolution(aMatchingSolution: Algorithm): void {
-    dispatch(clickOnMatchingSolution(aMatchingSolution));
-  },
-  dragExperiment(dragResult: DropResult): void {
-    dispatch(dragNDropAnExperiment(dragResult));
-  },
   loadExperiments(): void {
     dispatch(getExperiments()).then();
-  },
-  clickOnExperimentFilterTool(): void {
-    dispatch(clickOnExperimentsFilterTool());
   },
 });
 
