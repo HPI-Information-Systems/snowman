89490793d9cf9b2bdbad9f460ecbbf7745a28587
phpfs <phpfs@users.noreply.github.com>
2021-04-20 17:58:15 +0200

    Move DatasetCard and ExperimentCard (2)

--- app/src/apps/ExperimentsApp/ExperimentsApp.tsx
+++ app/src/apps/ExperimentsApp/ExperimentsApp.tsx
@@ -1,6 +1,6 @@
-import ExperimentsAppContainer from 'apps/ExperimentsPage/ExperimentsApp.Container';
-import { ExperimentsAppOwnProps } from 'apps/ExperimentsPage/ExperimentsAppProps';
-import { createExperimentsAppStore } from 'apps/ExperimentsPage/store/ExperimentsAppStoreFactory';
+import ExperimentsAppContainer from 'apps/ExperimentsApp/ExperimentsApp.Container';
+import { ExperimentsAppOwnProps } from 'apps/ExperimentsApp/ExperimentsAppProps';
+import { createExperimentsAppStore } from 'apps/ExperimentsApp/store/ExperimentsAppStoreFactory';
 import GenericSubApp from 'apps/SnowmanApp/components/GenericSubInstance/GenericSubApp/GenericSubApp';
 import React from 'react';
 import { ViewIDs } from 'types/ViewIDs';
