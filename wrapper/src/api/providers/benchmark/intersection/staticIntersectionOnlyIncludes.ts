61b21cd08a0f2285ff722e4c90bd9c6d44d16c46
Martin Graf <mgraf4@outlook.de>
2021-04-21 17:11:53 +0200

    remove unnecessary lines of code

--- wrapper/src/api/providers/benchmark/intersection/staticIntersectionOnlyIncludes.ts
+++ wrapper/src/api/providers/benchmark/intersection/staticIntersectionOnlyIncludes.ts
@@ -27,11 +27,7 @@ export class StaticIntersectionOnlyIncludes extends IntersectionOnlyIncludes {
       }
       return clustering;
     } else {
-      let splitIndex =
-        this.config.included.findIndex(({ similarity }) => similarity) + 1;
-      if (splitIndex === 0 || splitIndex === this.config.included.length) {
-        splitIndex = Math.floor(this.config.included.length / 2);
-      }
+      const splitIndex = Math.floor(this.config.included.length / 2);
       return SubclusterCache.get({
         datasetId: this.config.datasetId,
         base: this.config.included.slice(0, splitIndex),
