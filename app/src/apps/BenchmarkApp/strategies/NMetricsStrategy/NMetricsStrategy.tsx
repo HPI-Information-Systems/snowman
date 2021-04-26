f31f041ab0ddf423170da08293e0aefd36be48d2
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-26 11:05:47 +0200

    Use Magistrate for NMetricsStrategy.tsx

--- app/src/apps/BenchmarkApp/strategies/NMetricsStrategy/NMetricsStrategy.tsx
+++ app/src/apps/BenchmarkApp/strategies/NMetricsStrategy/NMetricsStrategy.tsx
@@ -1,6 +1,6 @@
 import GenericBenchmarkStrategy from 'apps/BenchmarkApp/components/GenericBenchmarkStrategy/GenericBenchmarkStrategy';
 import NMetricsStrategyContainer from 'apps/BenchmarkApp/strategies/NMetricsStrategy/NMetricsStrategy.Container';
-import { createNMetricsStrategyStore } from 'apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyStoreFactory';
+import { NMetricsStrategyStoreMagistrate } from 'apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyStoreFactory';
 import { loadStrategyData } from 'apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyThunkActions';
 import { StrategyIDs } from 'apps/BenchmarkApp/types/StrategyIDs';
 import React from 'react';
@@ -8,7 +8,9 @@ import React from 'react';
 const NMetricsStrategy = (): JSX.Element => (
   <GenericBenchmarkStrategy
     strategyId={StrategyIDs.NaryMetrics}
-    createStrategyStore={createNMetricsStrategyStore}
+    createStrategyStore={NMetricsStrategyStoreMagistrate.getStore.bind(
+      NMetricsStrategyStoreMagistrate
+    )}
     loadStrategyData={loadStrategyData}
   >
     <NMetricsStrategyContainer />
--- app/src/apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyStoreFactory.ts
+++ app/src/apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyStoreFactory.ts
@@ -1,14 +1,8 @@
 import NMetricsStrategyReducer from 'apps/BenchmarkApp/strategies/NMetricsStrategy/store/NMetricsStrategyReducer';
 import { NMetricsStrategyModel } from 'apps/BenchmarkApp/strategies/NMetricsStrategy/types/NMetricsStrategyModel';
-import { Store } from 'redux';
-import { SnowmanAction } from 'types/SnowmanAction';
-import { constructStore } from 'utils/storeFactory';
+import { StoreMagistrate } from 'utils/storeFactory';
 
-export const createNMetricsStrategyStore = (): Store<
-  NMetricsStrategyModel,
-  SnowmanAction
-> =>
-  constructStore<NMetricsStrategyModel>(
-    'NMetricsStrategyStore',
-    NMetricsStrategyReducer
-  );
+export const NMetricsStrategyStoreMagistrate = new StoreMagistrate<NMetricsStrategyModel>(
+  'NMetricsStrategyStore',
+  NMetricsStrategyReducer
+);
