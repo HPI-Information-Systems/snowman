ca3d782bd91bee981bfa12ad77e3d7ab1a85c02f
phpfs <phpfs@users.noreply.github.com>
2021-04-26 11:34:37 +0200

    Use StoreMagistrate for ExperimentsApp

--- app/src/apps/ExperimentsApp/ExperimentsApp.tsx
+++ app/src/apps/ExperimentsApp/ExperimentsApp.tsx
@@ -1,6 +1,6 @@
 import ExperimentsAppContainer from 'apps/ExperimentsApp/ExperimentsApp.Container';
 import { ExperimentsAppOwnProps } from 'apps/ExperimentsApp/ExperimentsAppProps';
-import { createExperimentsAppStore } from 'apps/ExperimentsApp/store/ExperimentsAppStoreFactory';
+import { ExperimentsAppMagistrate } from 'apps/ExperimentsApp/store/ExperimentsAppStoreFactory';
 import GenericSubApp from 'apps/SnowmanApp/components/GenericSubInstance/GenericSubApp/GenericSubApp';
 import React from 'react';
 import { ViewIDs } from 'types/ViewIDs';
@@ -9,7 +9,7 @@ const ExperimentsApp = (props: ExperimentsAppOwnProps): JSX.Element => (
   <GenericSubApp
     instanceId={ViewIDs.ExperimentsApp}
     appTitle="Experiments Editor"
-    createSubAppStore={createExperimentsAppStore}
+    createSubAppStore={ExperimentsAppMagistrate.getStore}
   >
     <ExperimentsAppContainer {...props} />
   </GenericSubApp>
--- app/src/apps/ExperimentsApp/store/ExperimentsAppStoreFactory.ts
+++ app/src/apps/ExperimentsApp/store/ExperimentsAppStoreFactory.ts
@@ -1,14 +1,8 @@
 import ExperimentsAppReducer from 'apps/ExperimentsApp/store/ExperimentsAppReducer';
 import { ExperimentsAppModel } from 'apps/ExperimentsApp/types/ExperimentsAppModel';
-import { Store } from 'redux';
-import { SnowmanAction } from 'types/SnowmanAction';
-import { constructStore } from 'utils/storeFactory';
+import { StoreMagistrate } from 'utils/storeFactory';
 
-export const createExperimentsAppStore = (): Store<
-  ExperimentsAppModel,
-  SnowmanAction
-> =>
-  constructStore<ExperimentsAppModel>(
-    'ExperimentsAppStore',
-    ExperimentsAppReducer
-  );
+export const ExperimentsAppMagistrate = new StoreMagistrate<ExperimentsAppModel>(
+  'ExperimentsAppStore',
+  ExperimentsAppReducer
+);
