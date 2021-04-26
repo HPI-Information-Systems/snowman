986b8a11570b29e582d95ba94bf2fdf85ab07b59
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-26 16:32:34 +0200

    Improve typing

--- app/src/utils/storeLink.tsx
+++ app/src/utils/storeLink.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
 import { SnowmanAction } from 'types/SnowmanAction';
 
-interface GenericProps {
-  [key: string]: never;
-}
-
 const storeLink = function (
   store: Store<unknown, SnowmanAction>,
-  AComponent: React.FC
-): (props: GenericProps) => JSX.Element {
+  AComponent: FC
+): (props: Record<string, unknown>) => JSX.Element {
   // eslint-disable-next-line react/display-name
-  return (props: GenericProps): JSX.Element => (
+  return (props: Record<string, unknown>): JSX.Element => (
     <Provider store={store}>
       <AComponent {...props} />
     </Provider>
