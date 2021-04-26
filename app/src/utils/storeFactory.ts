31d5da72c58578cf265729d5bce41a750a3a95e6
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-26 10:33:22 +0200

    Implement base magistrate

--- app/src/utils/storeFactory.ts
+++ app/src/utils/storeFactory.ts
@@ -3,6 +3,51 @@ import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import { SnowmanAction } from 'types/SnowmanAction';
 
+type StoreInstanceDescriptor = string;
+
+export class StoreMagistrate<StoreModel> {
+  static defaultID = 'Default';
+
+  poolName: string;
+  reducer: Reducer<StoreModel, SnowmanAction>;
+  pool: Map<string, Store<StoreModel, SnowmanAction>>;
+
+  constructor(poolName: string, reducer: Reducer<StoreModel, SnowmanAction>) {
+    this.pool = new Map<string, Store<StoreModel, SnowmanAction>>();
+    this.poolName = poolName;
+    this.reducer = reducer;
+  }
+
+  deleteStore(instanceDescriptor?: StoreInstanceDescriptor): boolean {
+    return this.pool.delete(instanceDescriptor ?? StoreMagistrate.defaultID);
+  }
+
+  private constructNewStore(
+    instanceDescriptor: StoreInstanceDescriptor
+  ): Store<StoreModel, SnowmanAction> {
+    const composeEnhancer = composeWithDevTools({
+      name: `${this.poolName}-${instanceDescriptor}`,
+    });
+    const newStore = createStore(
+      this.reducer,
+      composeEnhancer(applyMiddleware(thunk))
+    );
+    this.pool.set(instanceDescriptor, newStore);
+    return newStore;
+  }
+
+  getStore(
+    instanceDescriptor?: StoreInstanceDescriptor
+  ): Store<StoreModel, SnowmanAction> {
+    const usedInstanceDescriptor: StoreInstanceDescriptor =
+      instanceDescriptor ?? StoreMagistrate.defaultID;
+    return (
+      this.pool.get(usedInstanceDescriptor) ??
+      this.constructNewStore(usedInstanceDescriptor)
+    );
+  }
+}
+
 export const constructStore = <Model>(
   storeName: string,
   reducer: Reducer<Model, SnowmanAction>
