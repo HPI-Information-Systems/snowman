95cc3ee840136de7236ed12b5e7d755c4828c9da
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-26 11:40:23 +0200

    Remove obsolete pooled Factory

--- app/src/utils/storeFactory.ts
+++ app/src/utils/storeFactory.ts
@@ -67,38 +67,3 @@ export const dummyStoreFactory = (
   storeName: string
 ): (() => Store<unknown, SnowmanAction>) => (): Store<unknown, SnowmanAction> =>
   constructStore<null>(storeName, dummyReducer);
-
-export class PooledStoreFactory<StoreModel> {
-  static undefID = 'undef';
-  storePool: Map<string, Store<StoreModel, SnowmanAction>>;
-  storeBaseName: string;
-  reducer: Reducer<StoreModel, SnowmanAction>;
-
-  constructor(
-    storeBaseName: string,
-    reducer: Reducer<StoreModel, SnowmanAction>
-  ) {
-    this.storePool = new Map<string, Store<StoreModel, SnowmanAction>>();
-    this.storeBaseName = storeBaseName;
-    this.reducer = reducer;
-  }
-
-  getStore(instanceDescriptor?: string): Store<StoreModel, SnowmanAction> {
-    const existingStore = this.storePool.get(
-      instanceDescriptor ?? PooledStoreFactory.undefID
-    );
-    const resultingStore =
-      existingStore ??
-      constructStore(
-        `${this.storeBaseName} - ${
-          instanceDescriptor ?? PooledStoreFactory.undefID
-        }`,
-        this.reducer
-      );
-    this.storePool.set(
-      instanceDescriptor ?? PooledStoreFactory.undefID,
-      resultingStore
-    );
-    return resultingStore;
-  }
-}
