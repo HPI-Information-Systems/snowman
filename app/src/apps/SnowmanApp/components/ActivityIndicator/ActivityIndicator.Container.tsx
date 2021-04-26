77c66ce88cdce87fea440cb6e5b81978d5095bd5
phpfs <phpfs@users.noreply.github.com>
2021-04-26 13:42:27 +0200

    First version of storeConnect

--- app/src/apps/SnowmanApp/components/ActivityIndicator/ActivityIndicator.Container.tsx
+++ app/src/apps/SnowmanApp/components/ActivityIndicator/ActivityIndicator.Container.tsx
@@ -1,7 +1,9 @@
 import ActivityIndicatorView from 'apps/SnowmanApp/components/ActivityIndicator/ActivityIndicator.View';
 import { ActivityIndicatorStateProps } from 'apps/SnowmanApp/components/ActivityIndicator/ActivityIndicatorProps';
+import { SnowmanAppMagistrate } from 'apps/SnowmanApp/store/SnowmanAppStore';
 import { SnowmanAppModel } from 'apps/SnowmanApp/types/SnowmanAppModel';
 import { connect } from 'react-redux';
+import storeConnect from 'utils/storeConnect';
 
 const mapStateToProps = (
   state: SnowmanAppModel
@@ -12,5 +14,11 @@ const mapStateToProps = (
 const ActivityIndicatorContainer = connect(mapStateToProps)(
   ActivityIndicatorView
 );
+
+export const ActivityIndicator2 = storeConnect(
+  SnowmanAppMagistrate.getStore(),
+  mapStateToProps,
+  null
+)(ActivityIndicatorView);
 
 export default ActivityIndicatorContainer;
