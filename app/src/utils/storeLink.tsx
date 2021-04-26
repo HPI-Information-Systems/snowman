25c41e7d82d1fca8a6f7a5cdd74af586e5ca5987
phpfs <phpfs@users.noreply.github.com>
2021-04-26 16:43:55 +0200

    Allow for display name

--- app/src/utils/storeLink.tsx
+++ app/src/utils/storeLink.tsx
@@ -7,12 +7,14 @@ const storeLink = function (
   store: Store<unknown, SnowmanAction>,
   AComponent: FC
 ): (props: Record<string, unknown>) => JSX.Element {
-  // eslint-disable-next-line react/display-name
-  return (props: Record<string, unknown>): JSX.Element => (
+  const StoreProviderComponent = (
+    props: Record<string, unknown>
+  ): JSX.Element => (
     <Provider store={store}>
       <AComponent {...props} />
     </Provider>
   );
+  return StoreProviderComponent;
 };
 
 export default storeLink;
