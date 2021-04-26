13cf5920c022e8a9ec05726e91fffafe096f4a60
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-26 11:03:40 +0200

    Use Magistrate for BenchmarkApp

--- app/src/apps/BenchmarkApp/BenchmarkApp.tsx
+++ app/src/apps/BenchmarkApp/BenchmarkApp.tsx
@@ -1,6 +1,6 @@
 import BenchmarkAppContainer from 'apps/BenchmarkApp/BenchmarkApp.Container';
 import BenchmarkSelector from 'apps/BenchmarkApp/components/BenchmarkSelector/BenchmarkSelector';
-import { createBenchmarkAppStore } from 'apps/BenchmarkApp/store/BenchmarkAppStoreFactory';
+import { BenchmarkAppStoreMagistrate } from 'apps/BenchmarkApp/store/BenchmarkAppStoreFactory';
 import GenericSubApp from 'apps/SnowmanApp/components/GenericSubInstance/GenericSubApp/GenericSubApp';
 import React from 'react';
 import { ViewIDs } from 'types/ViewIDs';
@@ -9,7 +9,9 @@ const BenchmarkApp = (): JSX.Element => (
   <GenericSubApp
     instanceId={ViewIDs.BenchmarkApp}
     appTitle={'Benchmark Dashboard'}
-    createSubAppStore={createBenchmarkAppStore}
+    createSubAppStore={BenchmarkAppStoreMagistrate.getStore.bind(
+      BenchmarkAppStoreMagistrate
+    )}
     sideMenu={BenchmarkSelector}
     bringOwnPageStruct={true}
   >
--- app/src/apps/BenchmarkApp/store/BenchmarkAppStoreFactory.ts
+++ app/src/apps/BenchmarkApp/store/BenchmarkAppStoreFactory.ts
@@ -1,11 +1,8 @@
 import BenchmarkAppReducer from 'apps/BenchmarkApp/store/BenchmarkAppReducer';
 import { BenchmarkAppModel } from 'apps/BenchmarkApp/types/BenchmarkAppModel';
-import { Store } from 'redux';
-import { SnowmanAction } from 'store/messages';
-import { constructStore } from 'utils/storeFactory';
+import { StoreMagistrate } from 'utils/storeFactory';
 
-export const createBenchmarkAppStore = (): Store<
-  BenchmarkAppModel,
-  SnowmanAction
-> =>
-  constructStore<BenchmarkAppModel>('BenchmarkAppStore', BenchmarkAppReducer);
+export const BenchmarkAppStoreMagistrate = new StoreMagistrate<BenchmarkAppModel>(
+  'BenchmarkAppStore',
+  BenchmarkAppReducer
+);
