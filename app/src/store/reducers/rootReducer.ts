185e5211c8157b15d6a9f58cfc9aa37d0c1c826b
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-04-30 11:32:12 +0200

    Remove obsolete core store

--- app/src/store/reducers/rootReducer.ts
+++ app/src/store/reducers/rootReducer.ts
@@ -1,24 +1,17 @@
 import { SnowmanAction } from 'store/messages';
-import {
-  BenchmarkConfigurationStore,
-  CoreStore,
-  ImmediateStore,
-} from 'store/models';
+import { BenchmarkConfigurationStore, ImmediateStore } from 'store/models';
 import { BenchmarkConfiguratorReducer } from 'store/reducers/BenchmarkConfiguratorReducer';
 import { BinaryMetricsReducer } from 'store/reducers/BinaryMetricsReducer';
-import { CoreReducer } from 'store/reducers/CoreReducer';
 
 export const rootReducer = (
   state: ImmediateStore,
   action: SnowmanAction
 ): ImmediateStore => {
-  const coreState: CoreStore = CoreReducer(state?.CoreStore, action);
   const benchmarkState: BenchmarkConfigurationStore = BenchmarkConfiguratorReducer(
     state?.BenchmarkConfigurationStore
   );
 
   return {
-    CoreStore: coreState,
     BenchmarkConfigurationStore: benchmarkState,
     BinaryMetricsStore: BinaryMetricsReducer(state?.BinaryMetricsStore, action),
   };
