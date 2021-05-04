274744bfd88e2382fecb82e28edb4c7246820471
phpfs <phpfs@users.noreply.github.com>
2021-05-04 11:12:22 +0200

    Add store types

---
+++ app/src/apps/BenchmarkApp/components/BenchmarkConfigurator/components/SelectorPopoverGroup/types/SelectorPopoverGroupActionsTypes.ts
@@ -0,0 +1,4 @@
+export enum SelectorPopoverGroupActionsTypes {
+  OPEN_POPOVER = 'SelectorPopoverGroupActionsTypes-OPEN_POPOVER',
+  CLOSE_POPOVER = 'SelectorPopoverGroupActionsTypes-CLOSE_POPOVER',
+}
