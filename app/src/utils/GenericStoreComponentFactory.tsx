09bc02ae528e75bcb7986d779275422a58d1d0d5
phpfs <phpfs@users.noreply.github.com>
2021-05-04 17:20:21 +0200

    Refactor GenericStoreComponentFactory.tsx

--- app/src/utils/GenericStoreComponentFactory.tsx
+++ app/src/utils/GenericStoreComponentFactory.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Store } from 'redux';
-import { SnowmanAction } from 'types/SnowmanAction';
 import { StoreMagistrate } from 'utils/storeFactory';
 
 export interface GenericStoreComponentProps {
@@ -16,22 +14,9 @@ const GenericStoreComponentFactory = function <
   component: React.FC<OwnProps>
 ): typeof Component {
   return class GenericStoreComponent extends Component<OwnProps> {
-    store: Store<Model, SnowmanAction> | null = null;
-
-    UNSAFE_componentWillMount(): void {
-      this.store = magistrate.getStore(this.props.instanceDescriptor);
-    }
-
-    componentWillUnmount(): void {
-      this.store = null;
-    }
-
     render(): JSX.Element {
-      if (this.store === null) {
-        return <div />;
-      }
       return (
-        <Provider store={this.store}>
+        <Provider store={magistrate.getStore(this.props.instanceDescriptor)}>
           {React.createElement(component, this.props)}
         </Provider>
       );
