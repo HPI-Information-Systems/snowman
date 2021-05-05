1bd905ca9f714e5dd2432a6ac0d91a2dcd8d6865
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-05-05 18:29:14 +0200

    Implement ConstantStrategy.tsx

--- app/src/apps/FunctionBuilderDialog/components/ConstantStrategy/ConstantStrategy.tsx
+++ app/src/apps/FunctionBuilderDialog/components/ConstantStrategy/ConstantStrategy.tsx
@@ -1,6 +1,20 @@
-import { IonChip } from '@ionic/react';
-import React from 'react';
+import { IonChip, IonInput } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonChangeEvent } from 'types/IonChangeEvent';
+import { parseInputToNumberOrUndef } from 'utils/questionHelpers';
 
-const ConstantStrategy = (): JSX.Element => <IonChip>CONST</IonChip>;
+const ConstantStrategy = (): JSX.Element => {
+  const [constVal, setConstVal] = useState<number | undefined>(0);
+  return (
+    <IonChip>
+      <IonInput
+        value={constVal}
+        onIonChange={(event: IonChangeEvent): void =>
+          setConstVal(parseInputToNumberOrUndef(event.detail.value))
+        }
+      />
+    </IonChip>
+  );
+};
 
 export default ConstantStrategy;
