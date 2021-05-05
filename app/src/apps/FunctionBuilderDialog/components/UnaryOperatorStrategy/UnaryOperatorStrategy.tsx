3e7a66a79ad6068d70916dada6bff8e2338453c5
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-05-05 17:43:37 +0200

    Recursively define unary strategy

--- app/src/apps/FunctionBuilderDialog/components/UnaryOperatorStrategy/UnaryOperatorStrategy.tsx
+++ app/src/apps/FunctionBuilderDialog/components/UnaryOperatorStrategy/UnaryOperatorStrategy.tsx
@@ -1,10 +1,84 @@
-import { IonChip } from '@ionic/react';
-import React from 'react';
+import {
+  IonButton,
+  IonChip,
+  IonIcon,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
+import {
+  SimilarityThresholdFunctionDefinitionTypeEnum,
+  SimilarityThresholdFunctionUnaryOperatorOperatorEnum,
+} from 'api';
+import ConstantStrategy from 'apps/FunctionBuilderDialog/components/ConstantStrategy/ConstantStrategy';
+import OperatorStrategy from 'apps/FunctionBuilderDialog/components/OperatorStrategy/OperatorStrategy';
+import SimilarityThresholdStrategy from 'apps/FunctionBuilderDialog/components/SimilarityThresholdStrategy/SimilarityThresholdStrategy';
+import { UnaryOperatorStrategyProps } from 'apps/FunctionBuilderDialog/components/UnaryOperatorStrategy/UnaryOperatorStrategyProps';
+import { closeCircle } from 'ionicons/icons';
+import React, { useState } from 'react';
+import { IonChangeEvent } from 'types/IonChangeEvent';
 
-const UnaryOperatorStrategy = (): JSX.Element => (
-  <>
-    <IonChip>UnaryOP</IonChip>(<IonChip>VAL</IonChip>)
-  </>
-);
+const UnaryOperatorStrategy = ({
+  children,
+}: UnaryOperatorStrategyProps): JSX.Element => {
+  const [operator, setOperator] = useState(
+    SimilarityThresholdFunctionUnaryOperatorOperatorEnum.Acos as string
+  );
+  const [childType, setChildType] = useState('');
+  return (
+    <>
+      <IonChip>
+        <IonSelect
+          value={operator}
+          onIonChange={(event: IonChangeEvent): void =>
+            setOperator(event.detail.value as string)
+          }
+        >
+          {Object.keys(
+            SimilarityThresholdFunctionUnaryOperatorOperatorEnum
+          ).map(
+            (anOperator: string): JSX.Element => (
+              <IonSelectOption value={anOperator} key={anOperator}>
+                {anOperator}
+              </IonSelectOption>
+            )
+          )}
+        </IonSelect>
+      </IonChip>
+      (
+      {childType !== '' ? (
+        ((): JSX.Element => {
+          switch (childType) {
+            case SimilarityThresholdFunctionDefinitionTypeEnum.Operator:
+              return <OperatorStrategy />;
+            case SimilarityThresholdFunctionDefinitionTypeEnum.SimilarityThreshold:
+              return <SimilarityThresholdStrategy />;
+            case SimilarityThresholdFunctionDefinitionTypeEnum.UnaryOperator:
+              return <UnaryOperatorStrategy />;
+            default:
+              return <ConstantStrategy />;
+          }
+        })()
+      ) : (
+        <IonChip>
+          <IonSelect
+            value={childType}
+            onIonChange={(event: IonChangeEvent): void =>
+              setChildType(event.detail.value as string)
+            }
+          >
+            {Object.keys(SimilarityThresholdFunctionDefinitionTypeEnum).map(
+              (anOperatorType: string): JSX.Element => (
+                <IonSelectOption value={anOperatorType} key={anOperatorType}>
+                  {anOperatorType}
+                </IonSelectOption>
+              )
+            )}
+          </IonSelect>
+        </IonChip>
+      )}
+      <IonIcon icon={closeCircle} onClick={(): void => setChildType('')} />)
+    </>
+  );
+};
 
 export default UnaryOperatorStrategy;
