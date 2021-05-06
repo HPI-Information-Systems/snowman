7c7a22ecc272d654d1bb9fc9448cef5c6ee27871
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-05-06 11:34:14 +0200

    Prepare view for last strategy

--- app/src/apps/FunctionBuilderDialog/components/SimilarityThresholdStrategy/SimilarityThresholdStrategy.tsx
+++ app/src/apps/FunctionBuilderDialog/components/SimilarityThresholdStrategy/SimilarityThresholdStrategy.tsx
@@ -1,8 +1,17 @@
-import { IonChip } from '@ionic/react';
-import React from 'react';
+import { IonChip, IonSelect, IonSelectOption } from '@ionic/react';
+import React, { useState } from 'react';
 
-const SimilarityThresholdStrategy = (): JSX.Element => (
-  <IonChip>THRESHOLD</IonChip>
-);
+const SimilarityThresholdStrategy = (): JSX.Element => {
+  const [similarityThreshold, setSimilarityThreshold] = useState('a');
+  return (
+    <IonChip>
+      <IonSelect>
+        <IonSelectOption>a</IonSelectOption>
+        <IonSelectOption>b</IonSelectOption>
+        <IonSelectOption>c</IonSelectOption>
+      </IonSelect>
+    </IonChip>
+  );
+};
 
 export default SimilarityThresholdStrategy;
