1210373de8aec18c6e4d7d842619c86eabb764ca
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-05-06 15:58:57 +0200

    Migrate mapper to class component to handle next steps

--- app/src/apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapper.tsx
+++ app/src/apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapper.tsx
@@ -3,36 +3,49 @@ import {
   StrategyMap,
   StrategyMapItem,
 } from 'apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMap';
-import { StrategyMapperProps } from 'apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapperProps';
+import {
+  StrategyMapperProps,
+  StrategyMapperStateProps,
+} from 'apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapperProps';
 import StrategyUnselector from 'apps/FunctionBuilderDialog/components/StrategyUnselector/StrategyUnselector';
-import React, { createElement } from 'react';
+import React, { Component, createElement } from 'react';
 
-const StrategyMapper = ({
-  nextStrategyType,
-  setNextStrategyType,
-}: StrategyMapperProps): JSX.Element => (
-  <>
-    {((): JSX.Element => {
-      const targetStrategy = StrategyMap.find(
-        (aStrategyMapItem: StrategyMapItem): boolean =>
-          aStrategyMapItem.targetStrategyKey === nextStrategyType
-      )?.targetStrategyComponent;
-      return targetStrategy !== undefined ? (
-        <>
-          {createElement(targetStrategy)}
-          <StrategyUnselector
-            nextStrategyType={nextStrategyType}
-            setNextStrategyType={setNextStrategyType}
+class StrategyMapper extends Component<
+  StrategyMapperProps,
+  StrategyMapperStateProps
+> {
+  componentDidUpdate(prevProps: Readonly<StrategyMapperProps>): void {
+    if (prevProps.nextStrategyType !== this.props.nextStrategyType) {
+      this.setState({
+        ...this.state,
+        targetStrategy: StrategyMap.find(
+          (aStrategyMapItem: StrategyMapItem): boolean =>
+            aStrategyMapItem.targetStrategyKey === this.props.nextStrategyType
+        ),
+      });
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <>
+        {this.state?.targetStrategy !== undefined ? (
+          <>
+            {createElement(this.state.targetStrategy.targetStrategyComponent)}
+            <StrategyUnselector
+              nextStrategyType={this.props.nextStrategyType}
+              setNextStrategyType={this.props.setNextStrategyType}
+            />
+          </>
+        ) : (
+          <NextStrategySelector
+            nextStrategyType={this.props.nextStrategyType}
+            setNextStrategyType={this.props.setNextStrategyType}
           />
-        </>
-      ) : (
-        <NextStrategySelector
-          nextStrategyType={nextStrategyType}
-          setNextStrategyType={setNextStrategyType}
-        />
-      );
-    })()}
-  </>
-);
+        )}
+      </>
+    );
+  }
+}
 
 export default StrategyMapper;
--- app/src/apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapperProps.ts
+++ app/src/apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMapperProps.ts
@@ -1,3 +1,4 @@
+import { StrategyMapItem } from 'apps/FunctionBuilderDialog/components/StrategyMapper/StrategyMap';
 import { FunctionBuildingBlockType } from 'apps/FunctionBuilderDialog/types/FunctionBuildingBlock';
 
 export interface StrategyMapperForwardProps {
@@ -7,6 +8,10 @@ export interface StrategyMapperForwardProps {
 
 export interface StrategyMapperOwnProps {
   blockAccessKey?: number;
+}
+
+export interface StrategyMapperStateProps {
+  targetStrategy: StrategyMapItem | undefined;
 }
 
 export type StrategyMapperProps = StrategyMapperForwardProps &
