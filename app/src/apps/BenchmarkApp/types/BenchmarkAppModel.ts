439156cab490762b4b603b6e26934265a0e48324
phpfs <phpfs@users.noreply.github.com>
2021-05-06 14:12:16 +0200

    Remove old configurator (2)

--- app/src/apps/BenchmarkApp/types/BenchmarkAppModel.ts
+++ app/src/apps/BenchmarkApp/types/BenchmarkAppModel.ts
@@ -1,14 +1,11 @@
 import { Algorithm, Dataset, Experiment } from 'api';
 import { ConfigurationStoreModel } from 'apps/BenchmarkApp/types/ConfigurationStoreModel';
-import { ExpandedEntity } from 'apps/BenchmarkApp/types/ExpandedEntity';
 import { StrategyIDs } from 'apps/BenchmarkApp/types/StrategyIDs';
 import { SnowmanDispatch } from 'types/SnowmanDispatch';
 import { SnowmanThunkAction } from 'types/SnowmanThunkAction';
 
 export interface BenchmarkAppModel {
   resources: BenchmarkAppResourcesStore;
-  expandedAlgorithmsInDatasets: ExpandedEntity[];
-  searchString: string;
   activeStrategy: StrategyIDs;
   config: ConfigurationStoreModel;
 }
