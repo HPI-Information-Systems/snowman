c5e1c188f6f8a7b96d6aae00a446d2d4f06f2df2
phpfs <phpfs@users.noreply.github.com>
2021-05-07 13:18:45 +0200

    Demo scatter chart

--- app/src/apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategy.View.tsx
+++ app/src/apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategy.View.tsx
@@ -1,5 +1,5 @@
 import { KpiInvestigatorStrategyProps } from 'apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategyProps';
-import { LineChart } from 'components/simple/ChartComponent/LineChart';
+import { ScatterChart } from 'components/simple/ChartComponent/ScatterChart';
 import ErroneousBackdrop from 'components/simple/ErroneousBackdrop/ErroneousBackdrop';
 import React from 'react';
 
@@ -11,8 +11,33 @@ const KpiInvestigatorStrategyView = ({
       shouldShow={!isValidConfig}
       message={'Please select at least a one diagram track!'}
     />
-    <LineChart
-      data={{ datasets: [{ data: [1, 2, 3] }], labels: ['1', '2', '3'] }}
+    <ScatterChart
+      data={{
+        datasets: [
+          {
+            label: 'exp1',
+            backgroundColor: 'cyan',
+            data: [
+              { x: 3, y: 2 },
+              { x: 6, y: 3 },
+              { x: 10, y: 5 },
+              { x: 15, y: 8 },
+              { x: 18, y: 8 },
+            ],
+          },
+          {
+            label: 'exp2',
+            backgroundColor: 'purple',
+            data: [
+              { x: 5, y: 5 },
+              { x: 7, y: 2 },
+              { x: 9, y: 6 },
+              { x: 13, y: 9 },
+              { x: 16, y: 9 },
+            ],
+          },
+        ],
+      }}
     />
   </>
 );
