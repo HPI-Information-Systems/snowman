1dd074c4e0117aed69ac9852fce54a9b1bbf9eba
Martin Graf <mgraf4@outlook.de>
2021-05-10 14:53:08 +0200

    Correct syncinc with multi selects

--- app/src/apps/BenchmarkApp/components/BenchmarkConfigurator/components/AtomicSelectorGroups/MultiSelector/MultiSelector.View.tsx
+++ app/src/apps/BenchmarkApp/components/BenchmarkConfigurator/components/AtomicSelectorGroups/MultiSelector/MultiSelector.View.tsx
@@ -3,46 +3,57 @@ import { AtomicSelectorGroup } from 'apps/BenchmarkApp/components/BenchmarkConfi
 import { MultiSelectorProps } from 'apps/BenchmarkApp/components/BenchmarkConfigurator/components/AtomicSelectorGroups/MultiSelector/MultiSelectorProps';
 import styles from 'apps/BenchmarkApp/components/BenchmarkConfigurator/components/AtomicSelectorGroups/MultiSelector/MultiSelectorStyles.module.css';
 import { addCircle, closeCircleOutline } from 'ionicons/icons';
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactTooltip from 'react-tooltip';
 import style from 'theme/style';
 
 const MultiSelectorView = ({
   cacheKeys,
   push,
   remove,
-}: MultiSelectorProps): JSX.Element => (
-  <IonItem
-    className={style(
-      styles.itemNoSafePad,
-      styles.itemNoBorder,
-      styles.itemNoEndPad
-    )}
-  >
-    <IonList className={styles.list}>
-      {cacheKeys.map((cacheKey, index) => (
-        <IonItem
-          key={index}
-          className={style(
-            styles.itemNoBorder,
-            styles.itemResetEndPad,
-            styles.itemNoSafePad
-          )}
-        >
-          <AtomicSelectorGroup cacheKey={cacheKey} allowMultiple={false} />
-          <IonIcon
-            className={styles.pointer}
-            icon={closeCircleOutline}
-            color="danger"
-            onClick={() => remove(cacheKey)}
-            slot="end"
-          />
+}: MultiSelectorProps): JSX.Element => {
+  useEffect(() => {
+    // Triggered on every component update!
+    ReactTooltip.rebuild();
+  });
+  return (
+    <IonItem
+      className={style(
+        styles.itemNoSafePad,
+        styles.itemNoBorder,
+        styles.itemNoEndPad
+      )}
+    >
+      <IonList className={styles.list}>
+        {cacheKeys.map((cacheKey, index) => (
+          <IonItem
+            key={index}
+            className={style(
+              styles.itemNoBorder,
+              styles.itemResetEndPad,
+              styles.itemNoSafePad
+            )}
+          >
+            <AtomicSelectorGroup cacheKey={cacheKey} allowMultiple={false} />
+
+            <IonIcon
+              className={styles.pointer}
+              icon={closeCircleOutline}
+              color={index > 0 ? 'danger' : 'light'}
+              data-tip={
+                index === 0 ? 'The first item cannot be removed' : undefined
+              }
+              onClick={() => remove(cacheKey)}
+              slot="end"
+            />
+          </IonItem>
+        ))}
+        <IonItem button onClick={push}>
+          <IonIcon icon={addCircle} slot="end" color="primarydark" />
         </IonItem>
-      ))}
-      <IonItem button onClick={push}>
-        <IonIcon icon={addCircle} slot="end" color="primarydark" />
-      </IonItem>
-    </IonList>
-  </IonItem>
-);
+      </IonList>
+    </IonItem>
+  );
+};
 
 export default MultiSelectorView;
