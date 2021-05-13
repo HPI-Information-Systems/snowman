462690b32a1924644ebf934da187c6f73cbcaf1d
phpfs <phpfs@users.noreply.github.com>
2021-05-13 18:44:40 +0200

    Prepare view

--- app/src/apps/BenchmarkApp/strategies/SimilarityDiagramStrategy/SimilarityDiagramStrategy.View.tsx
+++ app/src/apps/BenchmarkApp/strategies/SimilarityDiagramStrategy/SimilarityDiagramStrategy.View.tsx
@@ -1,5 +1,73 @@
+import {
+  IonCard,
+  IonCol,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonRow,
+} from '@ionic/react';
+import styles from 'apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategyStyles.module.css';
+import { SimilarityDiagramStrategyProps } from 'apps/BenchmarkApp/strategies/SimilarityDiagramStrategy/SimilarityDiagramStrategyProps';
+import { ScatterChart } from 'components/simple/ChartComponent/ScatterChart';
+import ErroneousBackdrop from 'components/simple/ErroneousBackdrop/ErroneousBackdrop';
+import SelectableInput from 'components/stateful/SelectableInput/SelectableInput';
 import React from 'react';
+import {
+  AllMetricsObject,
+  OmitMetricsOnSoftKPIPage,
+} from 'types/AllMetricsEnum';
 
-const SimilarityDiagramStrategyView = (): JSX.Element => <div />;
+const SimilarityDiagramStrategyView = ({
+  isValidConfig,
+  yAxis,
+  datasets,
+  changeYAxis,
+}: SimilarityDiagramStrategyProps): JSX.Element => {
+  const metrics = Object.values(AllMetricsObject)
+    .filter((metric) => !OmitMetricsOnSoftKPIPage.has(metric))
+    .map((metric) => metric as string);
+  return (
+    <>
+      <ErroneousBackdrop
+        shouldShow={!isValidConfig}
+        message={
+          'Please select at least a one experiment with similarity values (or a function)!'
+        }
+      />
+      <IonGrid>
+        <IonRow>
+          <IonCol size="12">
+            <IonItem>
+              <IonLabel>Y Axis Metric:</IonLabel>
+              <SelectableInput
+                allOptions={metrics}
+                currentOption={yAxis}
+                setOption={changeYAxis}
+                instanceDescriptor="SimilarityDiagramYAxis"
+              />
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size="12">
+            <IonCard className={styles.noMargin}>
+              <div className={styles.chartContainer}>
+                <ScatterChart
+                  data={{
+                    datasets: datasets,
+                  }}
+                  options={{
+                    scales: { x: { min: 0, max: 1 }, y: { min: 0, max: 1 } },
+                    animation: { duration: 0 },
+                  }}
+                />
+              </div>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </>
+  );
+};
 
 export default SimilarityDiagramStrategyView;
--- app/src/apps/BenchmarkApp/strategies/SimilarityDiagramStrategy/SimilarityDiagramStrategyProps.ts
+++ app/src/apps/BenchmarkApp/strategies/SimilarityDiagramStrategy/SimilarityDiagramStrategyProps.ts
@@ -1,1 +1,15 @@
-export {};
+import { ScatterChartDatasets } from 'components/simple/ChartComponent/ScatterChart';
+import { AllMetricsEnum } from 'types/AllMetricsEnum';
+
+export interface SimilarityDiagramStrategyStateProps {
+  isValidConfig: boolean;
+  datasets: ScatterChartDatasets;
+  yAxis: AllMetricsEnum;
+}
+
+export interface SimilarityDiagramStrategyDispatchProps {
+  changeYAxis: (anOption: string) => void;
+}
+
+export type SimilarityDiagramStrategyProps = SimilarityDiagramStrategyStateProps &
+  SimilarityDiagramStrategyDispatchProps;
