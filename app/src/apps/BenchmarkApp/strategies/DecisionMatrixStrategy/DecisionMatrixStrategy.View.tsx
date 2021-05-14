3320843f75d8e74c543a0bca1d41c0091d32f69d
phpfs <phpfs@users.noreply.github.com>
2021-05-14 12:01:26 +0200

    Prepare table

--- app/src/apps/BenchmarkApp/strategies/DecisionMatrixStrategy/DecisionMatrixStrategy.View.tsx
+++ app/src/apps/BenchmarkApp/strategies/DecisionMatrixStrategy/DecisionMatrixStrategy.View.tsx
@@ -1,7 +1,33 @@
+import { IonCard } from '@ionic/react';
+import styles from 'apps/BenchmarkApp/strategies/DecisionMatrixStrategy/DecisionMatrixStrategyStyles.module.css';
+import ErroneousBackdrop from 'components/simple/ErroneousBackdrop/ErroneousBackdrop';
 import React from 'react';
 
 const DecisionMatrixStrategyView = (): JSX.Element => (
-  <div>DecisionMatrixStrategy</div>
+  <>
+    <ErroneousBackdrop
+      shouldShow={false}
+      message={
+        'Please select at least a single matching solution and, optionally, an experiment each.'
+      }
+    />
+    <IonCard>
+      <table className={styles.materialTable}>
+        <thead>
+          <tr>
+            <th>Spalte 1</th>
+            <th>Spalte 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Spalte 1</td>
+            <td>Spalte 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </IonCard>
+  </>
 );
 
 export default DecisionMatrixStrategyView;
