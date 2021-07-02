f70b1f8cd2a884740eca86a3dcbf0c2a4c233c80
florian-papsdorf <florian-papsdorf@users.noreply.github.com>
2021-07-02 10:57:47 +0200

    Hard code data points

--- app/src/apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategy.Container.tsx
+++ app/src/apps/BenchmarkApp/strategies/KpiInvestigatorStrategy/KpiInvestigatorStrategy.Container.tsx
@@ -24,12 +24,48 @@ const mapStateToProps = (
   state: KpiInvestigatorStrategyModel
 ): KpiInvestigatorStrategyStateProps => ({
   isValidConfig: state.isValidConfig,
-  datasets: state.diagramTracks.map(
+  datasets: [
+    {
+      name: 'Alpha',
+      data: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0.15 },
+        { x: 2.5, y: 0.3 },
+        { x: 4, y: 0.4 },
+        { x: 6, y: 0.49 },
+        { x: 8, y: 0.6 },
+        { x: 11, y: 0.7 },
+        { x: 14, y: 0.8 },
+        { x: 17, y: 0.85 },
+        { x: 22, y: 0.91 },
+        { x: 28, y: 0.95 },
+        { x: 40, y: 0.96 },
+      ],
+    },
+    {
+      name: 'Beta',
+      data: [
+        { x: 0, y: 0.3 },
+        { x: 1, y: 0.4 },
+        { x: 2.5, y: 0.5 },
+        { x: 4, y: 0.55 },
+        { x: 6, y: 0.65 },
+        { x: 8, y: 0.7 },
+        { x: 11, y: 0.75 },
+        { x: 13, y: 0.8 },
+        { x: 15, y: 0.88 },
+        { x: 16, y: 0.9 },
+        { x: 24, y: 0.92 },
+        { x: 30, y: 0.93 },
+        { x: 35, y: 0.94 },
+      ],
+    },
+  ].map(
     (diagramTrack, index): ScatterChartDataset => ({
       label: diagramTrack.name,
       backgroundColor: getMyColor(index, 0.7),
       borderColor: getMyColor(index),
-      data: sortBy(diagramTrack.coordinates.slice(), ({ x }) => x),
+      data: sortBy(diagramTrack.data.slice(), ({ x }) => x),
       pointRadius: 4,
       pointHoverRadius: 6,
       borderWidth: 1,
